0450df93bbde28869d7621f4ea01cfa5f056dda9
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-05-30 21:41:50 +0300

    Update install-icons.sh

--- install-icons.sh
+++ install-icons.sh
@@ -1,31 +1,39 @@
-    echo -e '\033[1;36m
+# https://github.com/m3tozz/FastCat 
+
+echo -e '\033[1;36m
     ______           __  ______      __ 
    / ____/___ ______/ /_/ ____/___ _/ /_
   / /_  / __ `/ ___/ __/ /   / __ `/ __/
  / __/ / /_/ (__  ) /_/ /___/ /_/ / /_  
 /_/    \__,_/____/\__/\____/\__,_/\__/  
- FastFetch Theme Pack                             
+ FastFetch Theme Pack                           
 \033[0m'
 
-FONT_DIR="$HOME/.fonts"
+read -p "Do you want to install CascadiaCode Nerd Font? (Y/N): " choice
 
-if [ ! -d "$FONT_DIR" ]; then
-    echo "Creating ~/.fonts directory..."
-    mkdir -p "$FONT_DIR"
-else
-    echo "~/.fonts directory already exists."
-fi
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+    FONT_DIR="$HOME/.fonts"
 
-echo "Downloading CascadiaCode Nerd Font..."
-wget -q -P "$FONT_DIR" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaCode.zip
+    if [ ! -d "$FONT_DIR" ]; then
+        echo -e "\033[1;32mCreating ~/.fonts directory...\033[0m"
+        mkdir -p "$FONT_DIR"
+    else
+        echo -e "\033[1;33m~/.fonts directory already exists.\033[0m"
+    fi
 
-echo "Extracting font files..."
-unzip -oq "$FONT_DIR/CascadiaCode.zip" -d "$FONT_DIR"
+    echo -e "\033[1;34mDownloading CascadiaCode Nerd Font...\033[0m"
+    wget -q -P "$FONT_DIR" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaCode.zip
 
-echo "Cleaning up..."
-rm "$FONT_DIR/CascadiaCode.zip"
+    echo -e "\033[1;34mExtracting font files...\033[0m"
+    unzip -oq "$FONT_DIR/CascadiaCode.zip" -d "$FONT_DIR"
 
-echo "Refreshing font cache..."
-fc-cache -fv
+    echo -e "\033[1;34mCleaning up...\033[0m"
+    rm "$FONT_DIR/CascadiaCode.zip"
+
+    echo -e "\033[1;34mRefreshing font cache...\033[0m"
+    fc-cache -fv
 
 echo -e "\nInstallation complete! Please open a new terminal and select 'CascadiaCove Nerd Font Regular' in your terminal settings."
+else
+    echo -e "\033[1;31mInstallation canceled by user.\033[0m"
+fi
