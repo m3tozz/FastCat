f8bb7f4762a9bb73df945069d22656c2fcc81c6f
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2025-07-18 00:04:45 +0300

    Update fastcat.sh

--- fastcat.sh
+++ fastcat.sh
@@ -186,7 +186,7 @@ echo -e '
     echo -e "$blue##############################################################$tp"
     echo -e "\nPress any key to continue..."
     read -r -n1
-	exit 1
+    
 elif [[ $islem == b || $islem == B ]]; then
 $SHELL ./backup.sh
 else
