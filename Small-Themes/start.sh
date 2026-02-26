c09dccdcf5f8520c9d70c9870f87722f89beec99
bulla0x <jyotishmanbaruah67@gmail.com>
2026-02-27 00:19:43 +0530

    bug fixed

--- Small-Themes/start.sh
+++ Small-Themes/start.sh
@@ -44,6 +44,7 @@ echo -e '\033[0;36m
 \e[1;33m[05]\e[0;32mArch \e[1;36m[06]\e[0;32mBlocks \e[1;34m[07]\e[0;32mCocktail \033[1;33m[08]\e[0;32mPalm \033[0;36m[09]\e[0;32mSheriff 
 \e[1;35m[10]\e[0;32mBunny \e[1;34m[11]\e[0;32mCoffee \e[1;35m[12]\e[0;32mDuck \033[1;33m[13]\e[0;32mOWL \e[1;34m[14]\e[0;32mGiraffe
 \e[1;36m[15]\e[0;32mRobo-Dog \e[1;34m[16]\e[0;32mTie-Fighter \e[1;36m[17]\e[0;32mWave
+\033[1;31m[x]Exit [00]Menu [D]Default-Theme
 '
         echo -ne "\e[1;33mm3tozz\e[0;31m@\033[1;34mfastcat\n\e[0;31m↳\e[1;36m " ; read islem
 }
